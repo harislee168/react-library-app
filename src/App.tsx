54ea359f0b750d2586c1094b62d8b251c0b2a025
Haris Lee <haris.lee168@gmail.com>
2023-08-09 11:53:15 +0800

    added okta login

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,32 @@ import Navbar from './layouts/NavbarAndFooter/Navbar';
 import Footer from './layouts/NavbarAndFooter/Footer';
 import HomePage from './layouts/HomePage/HomePage';
 import SearchBooksPage from './layouts/SearchBooksPage/SearchBooksPage';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import BookCheckoutPage from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { oktaConfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
+import { LoginCallback, Security } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWidget';
 
-function App() {
+const oktaAuth = new OktaAuth(oktaConfig);
+
+const App = () => {
+
+
+  const history = useHistory()
+  const customAuthHandler = () => {
+    history.push('/login')
+  }
+  const restoreOriginalUri = async( _oktaAuth: any, originalUri: any) => {
+    console.log('enter restore original uri')
+    console.log('_oktaAuth is:', _oktaAuth)
+    console.log('originalUri is:', originalUri)
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin))
+  }
+
   return (
     <div className='d-flex flex-column min-vh-100'>
+      <Security  oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
       <Navbar />
       <div className='flex-grow-1'>
         <Switch>
@@ -25,9 +45,12 @@ function App() {
           <Route path='/checkout/:bookId'>
             <BookCheckoutPage />
           </Route>
+          <Route path='/login' render={ () => <LoginWidget config={oktaConfig} />} />
+          <Route path='/login/callback' component={LoginCallback} />
         </Switch>
       </div>
       <Footer />
+      </Security>
     </div>
   );
 }
